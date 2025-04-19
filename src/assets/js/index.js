18636d11cae8053476752ef03d3127e786cca5c9
Saliho <medsalahbenjemia@gmail.com>
2025-04-19 12:45:48 +0100

    1.0.2ALPHA

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "✨Shine bright like a diamond✨"},
+            { "message": "Oh"},
+            { "message": "Diamonds..Idk" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
